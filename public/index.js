1d08eb7a8cbd629216374440db6d074bba641ae4
Hanachi <yarko-pryima@ukr.net>
2021-08-18 14:45:36 +0300

    add dynamic tooltip

--- public/index.js
+++ public/index.js
@@ -10,13 +10,18 @@ function initializeTable() {
 function createTable(data) {
   let table = document.getElementById('table')
   let row = document.createElement('tr');
+  let span = document.createElement("span");
 
   for(let key in data) {
     let col = document.createElement("td");
 
     col.innerHTML = data[key];
+    row.classList.add('tooltip');
 
     table.appendChild(row);
     row.appendChild(col);
   }
+  span.innerHTML = data.title;
+  span.classList.add('tooltip-text');
+  row.appendChild(span);
 }
