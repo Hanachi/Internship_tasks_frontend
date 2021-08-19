ac6eee6d2ff08d59fa405f9018c935922e76f992
Hanachi <yarko-pryima@ukr.net>
2021-08-19 14:02:08 +0300

    dynamic, static tooltips fixes

--- public/index.js
+++ public/index.js
@@ -10,18 +10,22 @@ function initializeTable() {
 function createTable(data) {
   let table = document.getElementById('table')
   let row = document.createElement('tr');
-  let span = document.createElement("span");
+
 
   for(let key in data) {
     let col = document.createElement("td");
-
+    let span = document.createElement("span");
+    
     col.innerHTML = data[key];
-    row.classList.add('tooltip');
+    col.classList.add('tooltip');
 
     table.appendChild(row);
+    col.appendChild(span);
     row.appendChild(col);
+
+    span.innerHTML = data.title;
+    span.classList.add('tooltip-text');
   }
-  span.innerHTML = data.title;
-  span.classList.add('tooltip-text');
-  row.appendChild(span);
+
+
 }
