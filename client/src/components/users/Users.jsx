aa456b7364af08a8fa05f29dc06bb7d1cc417ed2
Hanachi <yarko-pryima@ukr.net>
2022-03-15 16:55:20 +0200

    Improve UI for users tab

--- client/src/components/users/Users.jsx
+++ client/src/components/users/Users.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { Typography } from "@material-ui/core";
+
+import { Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 import { getUsers } from '../../api';
-import './Users.css'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+	usersContainer: {
+		display: 'flex',
+		justifyContent: 'center'
+	}
+}));
 
 const Users = () => {
+	const classes = useStyles();
 	const [users, setUsers] = useState();
 
 	useEffect(() => {
@@ -15,10 +33,35 @@ const Users = () => {
 	}, [])
 	
 	return (
-		<div className='users-container'>
-				<Typography className="users-paper">
-					{JSON.stringify(users)}
-				</Typography>
+		<div className={classes.usersContainer}>
+			<List className={classes.root}>
+				{users?.map((user) => {
+					const labelId = `checkbox-list-secondary-label-${user.id}`;
+					return (
+						<ListItem key={user.id} button>
+							<Grid container>
+								<Grid item xs={3}>
+								<ListItemAvatar>
+									<Avatar
+										alt={user.username}
+										src={user.username}
+									/>
+								</ListItemAvatar>
+								</Grid>
+								<Grid item xs={3}>
+									<ListItemText id={labelId} primary={user.username} />
+								</Grid>
+								<Grid item xs={3}>
+									<ListItemText primary={user.role} />
+								</Grid>
+								<Grid item xs={3}>
+									<ListItemText primary={user.email} />
+								</Grid>
+							</Grid>
+						</ListItem>
+					);
+				})}
+			</List>
 		</div>
 	)
 }
